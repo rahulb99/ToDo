73dc3c253544949e8c99367929044328badcb9cf
Rahul Baid <e0235283@u.nus.edu>
2019-02-26 13:44:45 +0800

    main.js: file created

--- app/src/main.js
+++ app/src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import store from './store'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import locale from 'element-ui/lib/locale/lang/en'
+Vue.use(ElementUI, { locale });
 
 new Vue({
-  render: h => h(App),
+  store,
+  render: h => h(App)
 }).$mount('#app')
